25e0f15da1224597ef676d79b52e887b9ca0fa31
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2023-06-24 14:05:25 +0400

    basic-api-auth

--- app/Http/Controllers/ApiTokenController.php
+++ app/Http/Controllers/ApiTokenController.php
@@ -2,18 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ApiTokenController extends Controller
 {
-    public function refresh(Request $request)
+    public function refresh(Request $request) // TODO to custom request & token expiration
     {
         $token = \Illuminate\Support\Str::random(60);
 
-        $request->user()->forceFill([
-            'api_token' => hash('sha256', $token),
-        ])->save();
+        $user = $this->getUser($request);
+        $user->api_token = hash('sha256', $token);
+        $user->save();
 
         return response()->json(['ok' => true, 'token' => $token]);
+    }
+
+    private function getUser(Request $request): User
+    {
+        $request->validate([
+            'login' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $login = $request->input('login');
+        $password = $request->input('password');
+
+        $user = User::query()->where('login', $login)->first();
+
+        if (empty($user)) {
+            throw new \InvalidArgumentException("Invalid login: {$login}");
+        }
+        if (!Hash::check($password, $user->password)) {
+            throw new \InvalidArgumentException("Invalid password: {$password}");
+        }
+        return $user;
     }
 }
