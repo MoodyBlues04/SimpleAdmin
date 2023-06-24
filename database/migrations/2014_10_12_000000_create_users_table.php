918566279c5f326c8adc06d6456012567febcb41
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2023-06-24 11:48:22 +0400

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('login');
+            $table->string('password');
             $table->string('name');
+            $table->string('surname');
+            $table->string('birthday')->nullable();
+            $table->integer('event_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2023_06_24_071111_events_table.php
+++ database/migrations/2023_06_24_071111_events_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('header');
+            $table->string('text');
+            $table->integer('creator_id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('events');
     }
 };
